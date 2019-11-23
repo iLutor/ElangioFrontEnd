dfa627aa463ed239fe41e46e7c11f4327fd8106d
sebastianit <sebastianacuna.it@gmail.com>
2019-11-22 22:53:35 -0300

    migration to mdb

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ContentComponent from './components/content/ContentComponent'
 import HeaderComponent from './components/header/HeaderComponent'
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 import './assets/scss/main.scss'
+
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 const App = () => {
     return (
