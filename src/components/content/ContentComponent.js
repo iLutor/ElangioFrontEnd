fdf50fb440cc47eb1d41e29e913ebb429a2dab5a
sebastianit <sebastianacuna.it@gmail.com>
2019-11-22 22:28:47 -0300

    test

--- src/components/content/ContentComponent.js
+++ src/components/content/ContentComponent.js
@@ -2,42 +2,32 @@ import React from 'react'
 import CardComponent from '../card/CardComponent'
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
 const ContentComponent = () => {
 
 
-    const rowStyle = {
-		marginTop: '20px',
-        marginBottom: '30px'
-    }
+   
 
     return (
         <>
-            <Grid container lg={12} spacing={1}>
-                <Grid container item xs={12} sm={12} lg={6} spacing={3}>
-                    <Grid item md={6} xs={12} sm={12} lg={4} >
-                        <CardComponent name="Pronunciation" />
-                    </Grid>
-                    <Grid item md={6} xs={12} sm={12} lg={6} >
-                        <CardComponent name="Pronunciation" />
-                    </Grid>
-                    <Grid item md={6} xs={12} sm={12} lg={6} >
-                        <CardComponent name="Listen" />
-                    </Grid>
-                </Grid >
-                <Grid container item xs={12} sm={12} lg={4} spacing={3}>
-                    <Grid item md={6} xs={12} sm={12} lg={4} >
-                        <CardComponent name="Chat" />
-                    </Grid>
-                    <Grid item md={6} xs={12} sm={12} lg={4} >
-                        <CardComponent name="Movie phrases" />
-                    </Grid>
-                    <Grid item md={6} xs={12} sm={12} lg={4} >
-                        <CardComponent name="Statistics" />
-                    </Grid>
-                </Grid >
-
+            <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+            >
+                <Grid item xs={12} lg={4}>
+                    1   
+                </Grid>
+                <Grid item xs={12} lg={4}>
+                    2
+                </Grid> 
+                <Grid item xs={12} lg={4}>
+                    3 
+                </Grid>
             </Grid>
+          
         </>
     )
 
